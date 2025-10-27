74b899228d9b589d01e4e33a7fbfd1660c892295
Arif Hermawan <muhammadarifhermawan4@gmail.com>
2025-10-27 07:43:53 +0700

    refactor: change default javascript confirm to custom style modal

--- views/create.php
+++ views/create.php
@@ -21,7 +21,7 @@ require_once '../utils/util.php';
         <p class="text-center text-gray-600">Form Tambah Data Pendaftaran Bimbingan Belajar</p>
       </div>
 
-      <form action="../controllers/applicantController.php" method="post" id="registrationForm"
+      <form action="../controllers/applicantController.php" method="post" id="createForm"
         class="flex flex-col w-full gap-4">
         <input type="hidden" name="action" value="create">
         <div class="flex flex-col md:flex-row w-full gap-3">
@@ -49,34 +49,34 @@ require_once '../utils/util.php';
             foreach ($data['packages'] as $package) {
               if ($package['id'] != 'undefined') {
                 ?>
-                <label for="<?php echo $package['id']; ?>"
+                <label for="<?= $package['id']; ?>"
                   class="pricing-card flex flex-col gap-2 w-full border border-black p-3.5 rounded-lg cursor-pointer">
-                  <input type="radio" name="packages" value="<?php echo $package['id']; ?>"
-                    id="<?php echo $package['id']; ?>" class="hidden" />
+                  <input type="radio" name="packages" value="<?= $package['id']; ?>" id="<?= $package['id']; ?>" onchange="
+                      document.getElementById('selectedPackage').innerHTML = '<?= $package['name'] ?>';
+                    " class="hidden" />
                   <div class="flex justify-between items-start">
-                    <span
-                      class="w-fit h-fit p-1 rounded-md <?php echo $package['iconBg']; ?> flex items-center justify-center">
+                    <span class="w-fit h-fit p-1 rounded-md <?= $package['iconBg']; ?> flex items-center justify-center">
                       <span style="font-size: xx-large;" class="material-symbols-outlined  text-white">
-                        <?php echo $package['iconName']; ?>
+                        <?= $package['iconName']; ?>
                       </span>
                     </span>
                     <?php if ($package['tag'] !== "") { ?>
-                      <span class="w-fit text-sm px-3 py-1 rounded-full <?php echo $package['tagBg']; ?>">
-                        <?php echo $package['tag']; ?>
+                      <span class="w-fit text-sm px-3 py-1 rounded-full <?= $package['tagBg']; ?>">
+                        <?= $package['tag']; ?>
                       </span>
                     <?php } ?>
                   </div>
                   <span class="">
-                    <p class="card-title text-xl font-semibold"><?php echo $package['name']; ?></p>
+                    <p class="card-title text-xl font-semibold"><?= $package['name']; ?></p>
                     <p class="text-gray-500">
-                      <?php echo $package['description']; ?>
+                      <?= $package['description']; ?>
                     </p>
                   </span>
                   <span class="flex gap-2 items-end">
                     <span>
                       <?php if ($package['originalPrice'] != $package['price'])
                         echo '<p class="text-gray-500 discounted-price">' . formatPrice($package['originalPrice']) . '</p>'; ?>
-                      <p class="font-bold text-3xl"><?php echo formatPrice($package['price']); ?></p>
+                      <p class="font-bold text-3xl"><?= formatPrice($package['price']); ?></p>
                     </span>
                     <p class="pb-2">/bulan</p>
                   </span>
@@ -100,10 +100,10 @@ require_once '../utils/util.php';
               <?php foreach ($data['addons'] as $addon) { ?>
                 <div class="flex items-center gap-2">
                   <input class="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500" type="checkbox"
-                    name="addons[]" value="<?php echo $addon['id']; ?>" id="<?php echo $addon['id']; ?>" />
-                  <label class="w-fit flex flex-row gap-2" for="<?php echo $addon['id']; ?>">
-                    <p class="font-medium"><?php echo $addon['name']; ?></p>
-                    <p class="text-gray-500"><?php echo formatPrice($addon['price'], "+"); ?></p>
+                    name="addons[]" value="<?= $addon['id']; ?>" id="<?= $addon['id']; ?>" />
+                  <label class="w-fit flex flex-row gap-2" for="<?= $addon['id']; ?>">
+                    <p class="font-medium"><?= $addon['name']; ?></p>
+                    <p class="text-gray-500"><?= formatPrice($addon['price'], "+"); ?></p>
                   </label>
                 </div>
               <?php } ?>
@@ -122,8 +122,8 @@ require_once '../utils/util.php';
                 class="block w-full rounded-md border-gray-300 py-2 px-3 shadow-sm focus:border-blue-500 focus:ring-blue-500"
                 aria-label="Default select example" name="locations">
                 <?php foreach ($data['locations'] as $location) { ?>
-                  <option value="<?php echo $location['id']; ?>">
-                    <?php echo $location['name'] . formatPrice($location['price'], "( +", ")"); ?>
+                  <option value="<?= $location['id']; ?>">
+                    <?= $location['name'] . formatPrice($location['price'], "( +", ")"); ?>
                   </option>
                 <?php } ?>
               </select>
@@ -135,8 +135,8 @@ require_once '../utils/util.php';
                 class="block w-full rounded-md border-gray-300 py-2 px-3 shadow-sm focus:border-blue-500 focus:ring-blue-500"
                 aria-label="Default select example" name="payments">
                 <?php foreach ($data['payments'] as $payment) { ?>
-                  <option value="<?php echo $payment['id']; ?>">
-                    <?php echo $payment['name'] . formatPrice($payment['price'], $payment['price'] > 0 ? " (+ " : "", $payment['price'] > 0 ? ")" : ""); ?>
+                  <option value="<?= $payment['id']; ?>">
+                    <?= $payment['name'] . formatPrice($payment['price'], $payment['price'] > 0 ? " (+ " : "", $payment['price'] > 0 ? ")" : ""); ?>
                   </option>
                 <?php } ?>
               </select>
@@ -164,20 +164,65 @@ require_once '../utils/util.php';
               class="border border-gray-500 px-3.5 py-2 rounded-md shadow-sm bg-gray-200 text-gray-800 hover:bg-gray-300 font-medium">
               Kosongkan formulir
             </button>
-            <button type="submit"
-              class="px-3.5 py-2 rounded-md shadow-sm bg-blue-600 text-white hover:bg-blue-700 font-medium">
+            <button id="shadowSubmitCreate" type="button" onclick="
+                document.getElementById('createForm').checkValidity()
+                  ? document.getElementById('triggerModalCreate').click()
+                  : document.getElementById('createSubmit').click();
+              " class="px-3.5 py-2 rounded-md shadow-sm bg-blue-600 text-white hover:bg-blue-700 font-medium">
               Tambahkan
             </button>
+            <button id="triggerModalCreate" type="button" command="show-modal" commandfor="confirm-submit"
+              class="hidden"></button>
+
+            <el-dialog>
+              <dialog id="confirm-submit" aria-labelledby="dialog-title"
+                class="fixed inset-0 size-auto max-h-none max-w-none overflow-y-auto bg-transparent backdrop:bg-transparent">
+                <el-dialog-backdrop
+                  class="fixed inset-0 bg-gray-500/75 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in"></el-dialog-backdrop>
+
+                <div tabindex="0"
+                  class="flex min-h-full items-end justify-center p-4 text-center focus:outline-none sm:items-center sm:p-0">
+                  <el-dialog-panel
+                    class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-lg data-closed:sm:translate-y-0 data-closed:sm:scale-95">
+                    <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                      <div class="sm:flex sm:items-start">
+                        <div
+                          class="mx-auto flex size-12 shrink-0 items-center justify-center rounded-full bg-green-100 sm:mx-0 sm:size-10">
+                          <span class="material-symbols-outlined">
+                            check_circle
+                          </span>
+                        </div>
+                        <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                          <h3 id="dialog-title" class="text-base font-semibold text-gray-900">Konfirmasi tambah data
+                          </h3>
+                          <div class="mt-2">
+                            <p class="text-sm text-gray-500">
+                              Anda memilih paket bimbel:
+                              <b id="selectedPackage" class="underline text-black"></b>.
+                              Apakah anda ingin melanjutkan?
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                      <button id="createSubmit" type="submit" command="close" commandfor="confirm-submit"
+                        class="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-medium text-white shadow-xs hover:bg-blue-500 sm:ml-3 sm:w-auto">
+                        Ya, tambahkan
+                      </button>
+                      <button type="button" command="close" commandfor="confirm-submit"
+                        class="mt-3 inline-flex w-full justify-center rounded-md bg-gray-200 px-3 py-2 text-sm font-medium text-gray-900 shadow-xs inset-ring inset-ring-gray-300 hover:bg-gray-300 sm:mt-0 sm:w-auto">Batal</button>
+                    </div>
+                  </el-dialog-panel>
+                </div>
+              </dialog>
+            </el-dialog>
           </div>
         </div>
       </form>
     </div>
   </div>
 
-  <script>
-    // js
-  </script>
-  <script src="../resources/main.js"></script>
 </body>
 
 </html>
